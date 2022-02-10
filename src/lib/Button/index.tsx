905c0eb2502945983b451514391bf7fc3e4b95c1
cqhaisenbao <fanyu9353@gmail.com>
2022-02-10 22:18:42 +0800

    添加button组件

--- src/lib/Button/index.tsx
+++ src/lib/Button/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import classNames from 'classnames';
 
-const Button = () => {
-  return <div>button</div>;
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  /**
+   * @description 按钮大小
+   */
+  size: 'lg' | 'sm';
+  buttonType: 'primary' | 'default' | 'danger' | 'link';
+  disabled?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = (props) => {
+  const { size, disabled, buttonType, children } = props;
+  const classess = classNames('dino-btn', {});
+  return <button>{children}</button>;
 };
 
 export default Button;
